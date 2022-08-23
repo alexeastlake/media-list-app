703520aa54a074f0bacaf8e1ef383c1daef91dac
alexeastlake <alex.eastlake@gmail.com>
2022-08-23 16:04:25 +1200

    Created seperate style file

--- library/components/ListItem.js
+++ library/components/ListItem.js
@@ -1,37 +1,17 @@
 import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import styles from './styles.js';
 
 export default function ListItem({item}) {
   let navigation = useNavigation();
   
   return (
-    <View style = {style.item}>
+    <View style = {styles.listItem}>
       <TouchableOpacity onPress = {() => navigation.navigate("Details", {item})}>
-        <Text style = {style.title}>{item.title}</Text>
-        <Text style = {style.text}>{item.platform}</Text>
-        <Text style = {style.text}>{item.genres.join(", ")}</Text>
+        <Text style = {styles.listItemTitle}>{item.title}</Text>
+        <Text style = {styles.listItemText}>{item.platform}</Text>
+        <Text style = {styles.listItemText}>{item.genres.join(", ")}</Text>
       </TouchableOpacity>
     </View>
   );
 }
-
-let style = StyleSheet.create({
-  item: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-    justifyContent: 'center',
-    
-    paddingHorizontal: 30,
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-  },
-
-  title: {
-    fontSize: 22.5,
-    fontWeight: "bold",
-  },
-
-  text: {
-    fontSize: 17.5,
-  },
-});
---
+++ library/components/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  listItem: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+  },
+
+  listItemTitle: {
+    fontSize: 22.5,
+    fontWeight: "bold",
+  },
+
+  listItemText: {
+    fontSize: 17.5,
+  },
+});
