41e36b2ebca518b1dd7f92478f889e8add10843c
alexeastlake <alex.eastlake@gmail.com>
2022-09-06 19:12:31 +1200

    Implemented loading of edit screen from Firebase

--- src/screens/main/ItemEditScreen.js
+++ src/screens/main/ItemEditScreen.js
@@ -1,32 +1,53 @@
-import React from "react";
-import {useRoute, useNavigation} from "@react-navigation/native";
+import React, { useContext, useState } from "react";
+import {useRoute, useNavigation, useFocusEffect} from "@react-navigation/native";
 import  {getItem, saveChange} from "../../utility/controller";
 import {Text, View, TextInput, KeyboardAvoidingView, ScrollView, Button} from "react-native";
 import styles from '../../../library/components/styles';
+import { UserContext } from "../../utility/UserContext";
 
 export default function ItemEditScreen({navigation, route}) {
-  let item = getItem(route.params.item.id);
+  let user = useContext(UserContext);
 
-  const [title, onChangeTitle] = React.useState(item.title);
-  const [platform, onChangePlatform] = React.useState(item.platform);
-  const [genres, onChangeGenres] = React.useState(item.genres.join(", "));
-  const [notes, onChangeNotes] = React.useState(item.notes);
+  const [item, setItem] = useState();
 
-  return (
-    <KeyboardAvoidingView style = {styles.screenContentWrapper} keyboardVerticalOffset = {-300} behavior = "padding">
-        <ScrollView>
-          <Text style = {styles.textInputTitle}>Title:</Text>
-          <TextInput style = {styles.textInput} onChangeText = {onChangeTitle} defaultValue = {title} multiline = {true}/>
-          <Text style = {styles.textInputTitle}>Platform/Service:</Text>
-          <TextInput style = {styles.textInput} onChangeText = {onChangePlatform} defaultValue = {platform} multiline = {true}/>
-          <Text style = {styles.textInputTitle}>Genres:</Text>
-          <TextInput style = {styles.textInput} onChangeText = {onChangeGenres} defaultValue = {genres} multiline = {true}/>
-          <Text style = {styles.textInputTitle}>Notes:</Text>
-          <TextInput style = {styles.textInput} onChangeText = {onChangeNotes} defaultValue = {notes} multiline = {true}/>
-          <Button title = "Save" onPress = {() => saveEdit(item.id, title, platform, genres, notes, navigation)}/>
-        </ScrollView>
-    </KeyboardAvoidingView>
+  useFocusEffect(
+    React.useCallback(() => {
+      getItem(user, route.params.item.id).then((result) => {
+        setItem(result);
+        onChangeTitle(result.title);
+        onChangePlatform(result.platform);
+        onChangeGenres(result.genres.join(", "));
+        onChangeNotes(result.notes);
+      });
+    }, [])
   );
+
+  const [title, onChangeTitle] = useState("");
+  const [platform, onChangePlatform] = useState("");
+  const [genres, onChangeGenres] = useState("");
+  const [notes, onChangeNotes] = useState("");
+
+  if (item) {
+    return (
+      <KeyboardAvoidingView style = {styles.screenContentWrapper} keyboardVerticalOffset = {-300} behavior = "padding">
+          <ScrollView>
+            <Text style = {styles.textInputTitle}>Title:</Text>
+            <TextInput style = {styles.textInput} onChangeText = {onChangeTitle} defaultValue = {title} multiline = {true}/>
+            <Text style = {styles.textInputTitle}>Platform/Service:</Text>
+            <TextInput style = {styles.textInput} onChangeText = {onChangePlatform} defaultValue = {platform} multiline = {true}/>
+            <Text style = {styles.textInputTitle}>Genres:</Text>
+            <TextInput style = {styles.textInput} onChangeText = {onChangeGenres} defaultValue = {genres} multiline = {true}/>
+            <Text style = {styles.textInputTitle}>Notes:</Text>
+            <TextInput style = {styles.textInput} onChangeText = {onChangeNotes} defaultValue = {notes} multiline = {true}/>
+            <Button title = "Save" onPress = {() => saveEdit(item.id, title, platform, genres, notes, navigation)}/>
+          </ScrollView>
+      </KeyboardAvoidingView>
+    );
+  } else {
+    return(
+      <Text>Loading...</Text>
+    );
+  }
 }
 
 function saveEdit(id, title, platform, genres, notes, navigation) {
